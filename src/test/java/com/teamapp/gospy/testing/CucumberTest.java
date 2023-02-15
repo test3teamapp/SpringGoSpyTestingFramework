1cb5ea8df9e8b512050a0c2fa4a25bedc2c7d37f
test3teamapp <test3teamapp@gmail.com>
2023-02-15 14:02:30 +0200

    JUnit Runner Class for Cucumber Tests added

--- src/test/java/com/teamapp/gospy/testing/CucumberTest.java
+++
@@ -1,8 +0,0 @@
-package com.teamapp.gospy.testing;
-
-
-public class CucumberTest {
-    /*
-    This class should be empty, step definitions should be in separate classes.
-     */
-}
---
+++ src/test/java/com/teamapp/gospy/testing/RunCucumberTests.java
@@ -0,0 +1,18 @@
+package com.teamapp.gospy.testing;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.teamapp.gospy.testing")
+public class RunCucumberTests {
+    /*
+    This class should be empty, step definitions should be in separate classes.
+     */
+}
